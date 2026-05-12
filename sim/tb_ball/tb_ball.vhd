9a1f64935814f460edf5f97e988f956987656f72
Michael Finn Jørgensen <mfj@weibel.dk>
2026-05-12 15:33:46 +0200

    Add tb_ball

---
+++ sim/tb_ball/tb_ball.vhd
@@ -0,0 +1,98 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use ieee.fixed_float_types.all;
+  use ieee.fixed_pkg.all;
+
+entity tb_ball is
+end entity tb_ball;
+
+architecture simulation of tb_ball is
+
+  signal   running : std_logic                  := '1';
+  signal   clk     : std_logic                  := '1';
+  signal   rst     : std_logic                  := '1';
+  signal   ce      : std_logic                  := '0';
+
+  constant C_POS_BITS : natural                 := 9;
+  constant C_VEL_BITS : natural                 := 9;
+  constant C_ACCURACY : natural                 := 8;
+  constant C_SCREEN_X : natural range 0 to 2047 := 256;
+  constant C_SCREEN_Y : natural range 0 to 2047 := 256;
+
+  signal   player_x   : ufixed(C_POS_BITS - 1 downto - C_ACCURACY);
+  signal   player_y   : ufixed(C_POS_BITS - 1 downto - C_ACCURACY);
+  signal   computer_x : ufixed(C_POS_BITS - 1 downto - C_ACCURACY);
+  signal   computer_y : ufixed(C_POS_BITS - 1 downto - C_ACCURACY);
+  signal   ball_pos_x : ufixed(C_POS_BITS - 1 downto - C_ACCURACY);
+  signal   ball_pos_y : ufixed(C_POS_BITS - 1 downto - C_ACCURACY);
+  signal   ball_vel_x : sfixed(C_VEL_BITS - 1 downto - C_ACCURACY);
+  signal   ball_vel_y : sfixed(C_VEL_BITS - 1 downto - C_ACCURACY);
+
+begin
+
+  clk <= running and not clk after 5 ns; -- 100 MHz
+  rst <= '1', '0' after 100 ns;
+
+  ball_inst : entity work.ball
+    generic map (
+      G_POS_BITS => C_POS_BITS,
+      G_VEL_BITS => C_VEL_BITS,
+      G_ACCURACY => C_ACCURACY,
+      G_SCREEN_X => C_SCREEN_X,
+      G_SCREEN_Y => C_SCREEN_Y
+    )
+    port map (
+      clk_i        => clk,
+      rst_i        => rst,
+      ce_i         => ce,
+      player_x_i   => player_x,
+      player_y_i   => player_y,
+      computer_x_i => computer_x,
+      computer_y_i => computer_y,
+      ball_pos_x_o => ball_pos_x,
+      ball_pos_y_o => ball_pos_y,
+      ball_vel_x_o => ball_vel_x,
+      ball_vel_y_o => ball_vel_y
+    ); -- ball_inst : entity work.ball
+
+
+  test_proc : process
+  begin
+    ce         <= '0';
+
+    wait until falling_edge(rst);
+    wait for 100 ns;
+    wait until rising_edge(clk);
+
+    report "Test started";
+    assert ball_pos_x = to_ufixed(128, C_POS_BITS - 1, - C_ACCURACY);
+    assert ball_pos_y = to_ufixed(128, C_POS_BITS - 1, - C_ACCURACY);
+    assert ball_vel_x = to_sfixed(0, C_POS_BITS - 1, - C_ACCURACY);
+    assert ball_vel_y = to_sfixed(0, C_POS_BITS - 1, - C_ACCURACY);
+
+    player_x   <= to_ufixed(100, C_POS_BITS - 1, - C_ACCURACY);
+    player_y   <= to_ufixed(200, C_POS_BITS - 1, - C_ACCURACY);
+    computer_x <= to_ufixed(200, C_POS_BITS - 1, - C_ACCURACY);
+    computer_y <= to_ufixed(200, C_POS_BITS - 1, - C_ACCURACY);
+    wait until rising_edge(clk);
+    ce         <= '1';
+    wait until rising_edge(clk);
+    ce         <= '0';
+    wait until rising_edge(clk);
+
+    wait for 1 us;
+    wait until rising_edge(clk);
+
+    assert ball_pos_x = to_ufixed(128, C_POS_BITS - 1, - C_ACCURACY);
+    assert ball_pos_y = to_ufixed(128, C_POS_BITS - 1, - C_ACCURACY);
+    assert ball_vel_x = to_sfixed(0, C_POS_BITS - 1, - C_ACCURACY);
+    assert ball_vel_y = to_sfixed(1, C_POS_BITS - 1, - C_ACCURACY);
+
+    report "Test finished";
+    running    <= '0';
+    wait;
+  end process test_proc;
+
+end architecture simulation;
+
--- src/ball.vhd
+++ src/ball.vhd
@@ -20,52 +20,89 @@ entity ball is
     player_y_i   : in    ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
     computer_x_i : in    ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
     computer_y_i : in    ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
-    ball_x_o     : out   ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
-    ball_y_o     : out   ufixed(G_POS_BITS - 1 downto - G_ACCURACY)
+    ball_pos_x_o : out   ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
+    ball_pos_y_o : out   ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
+    ball_vel_x_o : out   sfixed(G_VEL_BITS - 1 downto - G_ACCURACY);
+    ball_vel_y_o : out   sfixed(G_VEL_BITS - 1 downto - G_ACCURACY)
   );
 end entity ball;
 
 architecture synthesis of ball is
 
-  constant C_GRAVITY : natural  := 1;
+  constant C_GRAVITY : natural := 1;
 
-  constant C_MAX_POS : natural  := 2047;
-  constant C_MAX_VEL : natural  := 100;
+  type     state_type is (IDLE_ST, PLAYER_ST, COMPUTER_ST);
+  signal   state : state_type  := IDLE_ST;
 
-  subtype  POS_TYPE is ufixed(G_POS_BITS - 1 downto - G_ACCURACY);
-  subtype  VEL_TYPE is sfixed(G_VEL_BITS - 1 downto - G_ACCURACY);
+  signal   s_ready    : std_logic;
+  signal   s_valid    : std_logic;
+  signal   s_pos_x    : ufixed(G_POS_BITS - 1 downto -G_ACCURACY);
+  signal   s_pos_y    : ufixed(G_POS_BITS - 1 downto -G_ACCURACY);
+  signal   s_vel_x    : sfixed(G_VEL_BITS - 1 downto -G_ACCURACY);
+  signal   s_vel_y    : sfixed(G_VEL_BITS - 1 downto -G_ACCURACY);
+  signal   s_center_x : ufixed(G_POS_BITS - 1 downto -G_ACCURACY);
+  signal   s_center_y : ufixed(G_POS_BITS - 1 downto -G_ACCURACY);
 
-  signal   s_ready : std_logic;
-  signal   s_valid : std_logic;
-  signal   m_ready : std_logic;
   signal   m_valid : std_logic;
-
-  signal   pos_x     : POS_TYPE := to_ufixed(G_SCREEN_X / 2, G_POS_BITS - 1, - G_ACCURACY);
-  signal   pos_y     : POS_TYPE := to_ufixed(G_SCREEN_Y / 2, G_POS_BITS - 1, - G_ACCURACY);
-  signal   vel_x     : VEL_TYPE;
-  signal   vel_y     : VEL_TYPE;
-  signal   center_x  : POS_TYPE;
-  signal   center_y  : POS_TYPE;
-  signal   vel_x_new : VEL_TYPE;
-  signal   vel_y_new : VEL_TYPE;
+  signal   m_vel_x : sfixed(G_VEL_BITS - 1 downto -G_ACCURACY);
+  signal   m_vel_y : sfixed(G_VEL_BITS - 1 downto -G_ACCURACY);
 
 begin
 
-  -- Update ball velocity and position
-  ball_proc : process (clk_i)
+  fsm_proc : process (clk_i)
   begin
     if rising_edge(clk_i) then
-      if ce_i = '1' then
-        vel_x <= vel_x_new;
-        vel_y <= resize(vel_y_new + C_GRAVITY, vel_y);
+      if s_ready = '1' then
+        s_valid <= '0';
+      end if;
 
-        pos_x <= resize(ufixed(sfixed(pos_x) + vel_x), pos_x);
-        pos_y <= resize(ufixed(sfixed(pos_y) + vel_y), pos_y);
+      case state is
+
+        when IDLE_ST =>
+          if ce_i = '1' then
+            s_pos_x    <= s_pos_x;
+            s_pos_y    <= s_pos_y;
+            s_vel_x    <= s_vel_x;
+            s_vel_y    <= s_vel_y;
+            s_center_x <= player_x_i;
+            s_center_y <= player_y_i;
+            s_valid    <= '1';
+            state      <= PLAYER_ST;
+          end if;
+
+        when PLAYER_ST =>
+          if m_valid = '1' then
+            s_vel_x    <= m_vel_x;
+            s_vel_y    <= m_vel_y;
+            s_center_x <= computer_x_i;
+            s_center_y <= computer_y_i;
+            s_valid    <= '1';
+            state      <= COMPUTER_ST;
+          end if;
+
+        when COMPUTER_ST =>
+          if m_valid = '1' then
+            s_vel_x <= m_vel_x;
+            s_vel_y <= resize(m_vel_y + C_GRAVITY, s_vel_y);
+            s_pos_x <= resize(ufixed(sfixed(s_pos_x) + s_vel_x), s_pos_x);
+            s_pos_y <= resize(ufixed(sfixed(s_pos_y) + s_vel_y), s_pos_y);
+            state   <= IDLE_ST;
+          end if;
+
+      end case;
+
+      if rst_i = '1' then
+        s_valid <= '0';
+        s_pos_x <= to_ufixed(G_SCREEN_X / 2, G_POS_BITS - 1, - G_ACCURACY);
+        s_pos_y <= to_ufixed(G_SCREEN_Y / 2, G_POS_BITS - 1, - G_ACCURACY);
+        s_vel_x <= to_sfixed(0, G_VEL_BITS - 1, - G_ACCURACY);
+        s_vel_y <= to_sfixed(0, G_VEL_BITS - 1, - G_ACCURACY);
       end if;
     end if;
-  end process ball_proc;
+  end process fsm_proc;
 
-  -- Check collision with player
+
+  -- Check collision
   collision_inst : entity work.collision
     generic map (
       G_ACCURACY => G_ACCURACY,
@@ -76,22 +113,24 @@ begin
     port map (
       clk_i        => clk_i,
       rst_i        => rst_i,
-      s_ready_o    => open,
-      s_valid_i    => '1',
-      s_pos_x_i    => pos_x,
-      s_pos_y_i    => pos_y,
-      s_vel_x_i    => vel_x,
-      s_vel_y_i    => vel_y,
-      s_center_x_i => player_x_i,
-      s_center_y_i => player_y_i,
-      m_ready_i    => ce_i,
-      m_valid_o    => open,
-      m_vel_x_o    => vel_x_new,
-      m_vel_y_o    => vel_y_new
+      s_ready_o    => s_ready,
+      s_valid_i    => s_valid,
+      s_pos_x_i    => s_pos_x,
+      s_pos_y_i    => s_pos_y,
+      s_vel_x_i    => s_vel_x,
+      s_vel_y_i    => s_vel_y,
+      s_center_x_i => s_center_x,
+      s_center_y_i => s_center_y,
+      m_ready_i    => '1',
+      m_valid_o    => m_valid,
+      m_vel_x_o    => m_vel_x,
+      m_vel_y_o    => m_vel_y
     ); -- collision_inst : entity work.collision
 
-  ball_x_o <= pos_x;
-  ball_y_o <= pos_y;
+  ball_pos_x_o <= s_pos_x;
+  ball_pos_y_o <= s_pos_y;
+  ball_vel_x_o <= s_vel_x;
+  ball_vel_y_o <= s_vel_y;
 
 end architecture synthesis;
 
